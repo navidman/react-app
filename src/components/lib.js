5e07c3edaac853ebe1ec5384d47f3362c01fe890
navidman <navidmansourishsh@gmail.com>
2022-05-06 19:51:45 +0430

    dynamic colors and media queries added

--- src/components/lib.js
+++ src/components/lib.js
@@ -1,14 +1,16 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled/macro'
 import {Dialog as ReachDialog} from '@reach/dialog'
+import * as colors from '../styles/colors'
+import * as mq from '../styles/media-queries'
 
 const buttonVariants = {
   primary: {
-    background: '#3f51b5',
-    color: 'white',
+    background: colors.indigo,
+    color: colors.base,
   },
   secondary: {
-    background: '#f1f2f7',
-    color: '#434449',
+    background: colors.gray,
+    color: colors.text,
   },
 }
 const Button = styled.button(
@@ -17,15 +19,19 @@ const Button = styled.button(
     border: '0',
     lineHeight: '1',
     borderRadius: '3px',
-  },
-  ({variant = 'primary'}) => buttonVariants[variant],
+  },({variant = 'primary'}) => buttonVariants[variant],
 )
 
 const Input = styled.input({
   borderRadius: '3px',
-  border: '1px solid #f1f1f4',
-  background: '#f1f2f7',
+  border: `1px solid ${colors.gray}`,
+  background: colors.gray,
   padding: '8px 12px',
+})
+
+const FormGroup = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
 })
 
 const CircleButton = styled.button({
@@ -37,9 +43,9 @@ const CircleButton = styled.button({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  background: 'white',
-  color: '#434449',
-  border: `1px solid #f1f1f4`,
+  background: colors.gray,
+  color: colors.text,
+  border: `1px solid ${colors.gray10}`,
   cursor: 'pointer',
 })
 
@@ -49,15 +55,10 @@ const Dialog = styled(ReachDialog)({
   paddingBottom: '3.5em',
   boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
   margin: '20vh auto',
-  '@media (max-width: 991px)': {
+  [mq.small]: {
     width: '100%',
     margin: '10vh auto',
   },
 })
 
-const FormGroup = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-})
-
-export {Button, Input, CircleButton, Dialog, FormGroup}
+export {CircleButton, Dialog, Button, Input, FormGroup}
